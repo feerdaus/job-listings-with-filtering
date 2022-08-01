8e102e3500c6124a9333f84fb68deefcb02265ee
feerdaus <firdauslawal@gmail.com>
2022-08-01 23:41:48 +0100

    Initialize project

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+import * as React from "react";
+import Button from "@mui/material/Button";
+
+const Home = () => {
+  return <Button variant="contained">Hello World</Button>;
+};
+
+export default Home;
